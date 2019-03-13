a4fcdf177a7571a0657c463d382fcfdd376ff8b8
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-03-13 10:55:20 -0400

    Add update to user message to clear message

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,7 +4,7 @@ const store = require('../store.js')
 
 const resetMessage = function () {
   setTimeout(() => {
-    $('#user-message').text('Try Something New!')
+    $('#user-message').text('')
   }, 3000)
 }
 
